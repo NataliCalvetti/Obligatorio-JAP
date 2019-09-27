b546bc5cc1e59180d5d79c5673d5ec7d4ccfcbc9
Natali Calvetti <53353282+NataliCalvetti@users.noreply.github.com>
2019-09-27 11:38:30 -0300

    google3435

--- autentication.js
+++ autentication.js
@@ -27,7 +27,6 @@ document.querySelector('button').addEventListener('click', (event) =>{
     document.querySelector('.mensaje-contraseña').style.display = 'none';
     document.querySelector('.mensaje-usuario').style.display = 'none';
     document.querySelector('.mensaje').style.display = 'none';
-    document.querySelector('.mensaje-google').style.display = 'none'
 
         let nameInput = document.getElementById('name').value;
         let passwordInput = document.getElementById('password').value;
@@ -58,41 +57,7 @@ document.querySelector('button').addEventListener('click', (event) =>{
    
 })
 
-function onSignIn(googleUser) {
-    console.log(googleUser);
-    debugger;
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log(id_token);
 
-   const googleAutentication = [ 
-        {
-            id: profile.getId(),
-            name: profile.getName(),
-            email: profile.getEmail()
-        }
-    ];
-
-    if(googleAutentication){
-        localStorage.setItem('usuario', googleAutentication.name);
-                   window.location.href = 'index.html';
-    }else {
-            document.querySelector('.mensaje-google').style.display = 'inline-block';
-
-    }
-
-  }
-
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
 
 
 
---
+++ script.js
@@ -0,0 +1,61 @@
+function onSignIn(googleUser) {
+    gapi.load('auth2', function() {
+        auth2 = gapi.auth2.init({
+          client_id: '647412047104-pfh3cjaqv6dc91ur84hnkjf9ltb8mstl.apps.googleusercontent.com',
+          fetch_basic_profile: false,
+          scope: 'profile'
+        });
+      
+        // Sign the user in, and then retrieve their ID.
+      });
+
+      function whenClick() {
+        auth2.signIn().then(function() {
+            console.log(auth2.currentUser.get().getId());
+          });
+      }
+      
+   /*const googleAutentication = [ 
+        {
+            id: profile.getId(),
+            name: profile.getName(),
+            email: profile.getEmail()
+        }
+    ];
+
+    if(googleAutentication){
+        localStorage.setItem('usuario', googleAutentication.name);
+                   window.location.href = 'index.html';
+    }else {
+            document.querySelector('.mensaje-google').style.display = 'inline-block';
+
+    }
+
+  }*/
+
+  function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  }
+  function onFailure(error) {
+    console.log(error);
+  }
+  function renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': onSuccess,
+      'onfailure': onFailure
+    });
+  
+
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
