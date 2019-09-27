182ee33694202838ff12f1d999976b219de977cd
Natali Calvetti <53353282+NataliCalvetti@users.noreply.github.com>
2019-09-27 14:54:00 -0300

    asdasddsd

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function onSignIn(googleUser) {
+async function onSignIn(googleUser) {
     document.querySelector('.mensaje-google').style.display = 'none';
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
