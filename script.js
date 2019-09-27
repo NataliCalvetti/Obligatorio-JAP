6153ec6d6e0739e4cfda0266d72b1bf39aa8f534
Natali Calvetti <53353282+NataliCalvetti@users.noreply.github.com>
2019-09-27 14:36:20 -0300

    sfsdfsdf

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function onSignIn(googleUser) {
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    var id_token = googleUser.getAuthResponse().id_token;
+    var id_token = await googleUser.getAuthResponse().id_token;
     console.log(id_token);
 
     if(id_token){
