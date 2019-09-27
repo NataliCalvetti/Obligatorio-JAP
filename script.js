16f95b2793fbf91c5820c88632a4bb035e6fa748
Natali Calvetti <53353282+NataliCalvetti@users.noreply.github.com>
2019-09-27 12:53:02 -0300

    adasdas

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
 function onSignIn(googleUser) {
-    gapi.load('auth2', function() {
-        auth2 = gapi.auth2.init({
-          client_id: '647412047104-pfh3cjaqv6dc91ur84hnkjf9ltb8mstl.apps.googleusercontent.com',
-          fetch_basic_profile: false,
-          scope: 'profile'
-        });
-  });
-
-  var profile = googleUser.getBasicProfile();
+    var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present. 
-}
-
-
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
