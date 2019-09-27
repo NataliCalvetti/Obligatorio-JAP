8c531911cc760ea883e24b2ffd6264fc65c2d960
Natali Calvetti <53353282+NataliCalvetti@users.noreply.github.com>
2019-09-27 12:23:31 -0300

    listeners

--- script.js
+++ script.js
@@ -1,61 +1,30 @@
-function onSignIn(googleUser) {
-    gapi.load('auth2', function() {
-        auth2 = gapi.auth2.init({
-          client_id: '647412047104-pfh3cjaqv6dc91ur84hnkjf9ltb8mstl.apps.googleusercontent.com',
-          fetch_basic_profile: false,
-          scope: 'profile'
-        });
-      }
-      );
-    }
-      
-
-      function whenClick() {
-        auth2.signIn().then(function() {
-            console.log(auth2.currentUser.get().getId());
-          });
-      }
-      
-   /*const googleAutentication = [ 
-        {
-            id: profile.getId(),
-            name: profile.getName(),
-            email: profile.getEmail()
-        }
-    ];
-
-    if(googleAutentication){
-        localStorage.setItem('usuario', googleAutentication.name);
-                   window.location.href = 'index.html';
-    }else {
-            document.querySelector('.mensaje-google').style.display = 'inline-block';
-
-    }
-
-  }*/
-
-  function onSuccess(googleUser) {
-    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-  }
-  function onFailure(error) {
-    console.log(error);
-  }
-  function renderButton() {
-    gapi.signin2.render('my-signin2', {
-      'scope': 'profile email',
-      'width': 240,
-      'height': 50,
-      'longtitle': true,
-      'theme': 'dark',
-      'onsuccess': onSuccess,
-      'onfailure': onFailure
-    });
-  
+function onSignIn() {
+    var auth2; // The Sign-In object.
+var googleUser; // The current user.
 
 
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }}
+/**
+ * Calls startAuth after Sign in V2 finishes setting up.
+ */
+var appStart = function() {
+  gapi.load('auth2', initSigninV2);
+};
+
+
+/**
+ * Initializes Signin v2 and sets up listeners.
+ */
+var initSigninV2 = function() {
+  auth2 = gapi.auth2.init({
+      client_id: 'CLIENT_ID.apps.googleusercontent.com',
+      scope: 'profile'
+  });
+
+  // Listen for sign-in state changes.
+  auth2.isSignedIn.listen(signinChanged);
+
+  var signinChanged = function (val) {
+    console.log('Signin state changed to ', val);
+    document.getElementById('signed-in-cell').innerText = val;
+  };
+}}
