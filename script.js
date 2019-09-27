0f87c921bf98d725112f3c28333564a46bcfe6be
Natali Calvetti <53353282+NataliCalvetti@users.noreply.github.com>
2019-09-27 14:55:17 -0300

    asdasda

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-async function onSignIn(googleUser) {
+function onSignIn(googleUser) {
     document.querySelector('.mensaje-google').style.display = 'none';
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    var id_token = await googleUser.getAuthResponse().id_token;
+    var id_token = googleUser.getAuthResponse().id_token;
     console.log(id_token);
 
     if(id_token){
