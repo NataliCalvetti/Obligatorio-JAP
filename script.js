baffcd45a4118156a2a0ef5d5ae870de68953954
Natali Calvetti <53353282+NataliCalvetti@users.noreply.github.com>
2019-09-28 14:21:34 -0300

    test

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
- async function onSignIn(googleUser) {
+ async function onSignIn() {
+    gapi.client.init({
+        'clientId': '647412047104-pfh3cjaqv6dc91ur84hnkjf9ltb8mstl.apps.googleusercontent.com',
+        'scope': "profile"
+    }).then(function () {
+      gapi.auth2.onSignIn().then( (response) => {
+        console.log(response);
+      });
+
+      /* // Listen for sign-in state changes.
+      GoogleAuth.isSignedIn.listen(updateSigninStatus);
+
+      // Handle initial sign-in state. (Determine if user is already signed in.)
+      var user = GoogleAuth.currentUser.get();
+      setSigninStatus();
+
     document.querySelector('.mensaje-google').style.display = 'none';
     const localStorageName = localStorage.getItem('usuario');
     var profile = await googleUser.getBasicProfile();
@@ -10,5 +25,5 @@
         window.location.href = 'index.html';
     } else {
         document.querySelector('.mensaje-google').style.display = 'inline-block';
-    }
-}
+    } */
+    })};
