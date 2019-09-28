e826ed69b5edcc89ff7144534b9ca7335db3babc
Natali Calvetti <53353282+NataliCalvetti@users.noreply.github.com>
2019-09-28 13:26:41 -0300

    test

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
  async function onSignIn(googleUser) {
     document.querySelector('.mensaje-google').style.display = 'none';
-    var profile = googleUser.getBasicProfile();
+    var profile = await googleUser.getBasicProfile();
     var id_token = await googleUser.getAuthResponse().id_token;
 
     if(id_token){
         const googleUsername = await profile.getName();
         localStorage.setItem('usuario', googleUsername);
-        window.location.href = 'index.html';
+        console.log(profile);
     } else {
         document.querySelector('.mensaje-google').style.display = 'inline-block';
     }
