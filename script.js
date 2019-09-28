afd90b6c1c9e6b41769457eba947813687c2a8a7
Natali Calvetti <53353282+NataliCalvetti@users.noreply.github.com>
2019-09-28 14:44:25 -0300

    test

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
  async function onSignIn() {
-    await gapi.load('auth2', () => {
-        gapi.auth2.init({
-            'clientId': '647412047104-pfh3cjaqv6dc91ur84hnkjf9ltb8mstl.apps.googleusercontent.com',
-        })
-      }).then(async (response) => {
-          console.log(response);
-          
-                await gapi.auth2.onSignIn().then((response) => {
-                    console.log(response);
-                });
-      });
-
-      /* 
-
-      // Listen for sign-in state changes.
-      GoogleAuth.isSignedIn.listen(updateSigninStatus);
-
-      // Handle initial sign-in state. (Determine if user is already signed in.)
-      var user = GoogleAuth.currentUser.get();
-      setSigninStatus();
-
     document.querySelector('.mensaje-google').style.display = 'none';
-    const localStorageName = localStorage.getItem('usuario');
     var profile = await googleUser.getBasicProfile();
     var id_token = await googleUser.getAuthResponse().id_token;
 
@@ -32,7 +10,4 @@
     } else {
         document.querySelector('.mensaje-google').style.display = 'inline-block';
     } 
-})};
-   */
-
 }
