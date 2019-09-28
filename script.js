7856108d8f10e169041caa5c87a8c29853d04319
Natali Calvetti <53353282+NataliCalvetti@users.noreply.github.com>
2019-09-28 13:20:28 -0300

    test

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 function onSignIn(googleUser) {
     document.querySelector('.mensaje-google').style.display = 'none';
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     var id_token = googleUser.getAuthResponse().id_token;
-    console.log(id_token);
 
     if(id_token){
-        localStorage.setItem('usuario', profile.getName());
+        const googleUsername = profile.getName();
+        localStorage.setItem('usuario', googleUsername);
         window.location.href = 'index.html';
     } else {
         document.querySelector('.mensaje-google').style.display = 'inline-block';
