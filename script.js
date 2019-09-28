f7450064cb4eeba6a3c99ca848ab6049cb2e843f
Natali Calvetti <53353282+NataliCalvetti@users.noreply.github.com>
2019-09-28 14:14:35 -0300

    test

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
  async function onSignIn(googleUser) {
-     document.querySelector('.mensaje-google').style.display = 'none';
-     gapi.auth2.init({
-        client_id: 'CLIENT_ID.apps.googleusercontent.com',
-        scope: 'profile'
-     });
-     gapi.auth2.getAuthInstance();
+    document.querySelector('.mensaje-google').style.display = 'none';
+    const localStorageName = localStorage.getItem('usuario');
+    var profile = await googleUser.getBasicProfile();
+    var id_token = await googleUser.getAuthResponse().id_token;
 
-     gapi.auth2.onSignIn( async () => {
-         var profile = await googleUser.getBasicProfile();
-         var id_token = await googleUser.getAuthResponse().id_token;
-         if(id_token){
+    if(id_token){
         const googleUsername = await profile.getName();
         localStorage.setItem('usuario', googleUsername);
-        console.log(profile);
-        } else {
+        window.location.href = 'index.html';
+    } else {
         document.querySelector('.mensaje-google').style.display = 'inline-block';
-        }
-     })
-
+    }
 }
