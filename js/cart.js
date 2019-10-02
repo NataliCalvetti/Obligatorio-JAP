7628e5c60bdd96dd6ddde1cce85c40bb865e8c87
Natali Calvetti <53353282+NataliCalvetti@users.noreply.github.com>
2019-10-02 19:51:26 -0300

    terceraparte

--- js/cart.js
+++ js/cart.js
@@ -28,78 +28,21 @@ function hidePaymentTypeNotSelected(){
 
 function showArticles(articles){
     const htmlContentToAppend = `
-    <main role="main">
-    <div class="text-center p-4">
+    <div class="container mt-5">
+        <div class="text-center p-4">
             <h2>Mi carrito</h2>
-    <div class="container mt-5"></div>
-    <div class="modal fade" tabindex="-1" role="dialog" id="contidionsModal">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title">Forma de pago</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <div class="container">
-                    <div class="row">
-                      <div class="custom-control custom-radio">
-                        <input id="creditCardPaymentRadio" name="paymentType" type="radio" class="custom-control-input">
-                        <label class="custom-control-label" for="creditCardPaymentRadio">Tarjeta de crédito</label>
-                      </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                      <div class="col-md-6 mb-3">
-                        <label for="shippingStreet">Número de tarjeta</label>
-                        <input type="text" class="form-control" id="creditCardNumber" placeholder="" value="" >
-                        <div class="invalid-feedback">
-                          Ingresa un número de tarjeta
-                        </div>
-                      </div>
-                      <div class="col-md-4 mb-3">
-                        <label for="shippingStreetNumber">Código de seg.</label>
-                        <input type="number" class="form-control" id="creditCardSecurityCode" placeholder="" value="" >
-                        <div class="invalid-feedback">
-                          Ingresa el código de seguridad
-                        </div>
-                      </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6 mb-3">
-                          <label for="dueDate">Vencimiento (MM/AA)</label>
-                          <input type="text" class="form-control" id="dueDate" placeholder="" value="" >
-                          <div class="invalid-feedback">
-                            Ingresa la fecha de vencimiento
-                          </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                      <div class="custom-control custom-radio">
-                        <input id="bankingRadio" name="paymentType" type="radio" class="custom-control-input">
-                        <label class="custom-control-label" for="bankingRadio">Transferencia bancaria</label>
-                      </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                      <div class="col-md-6 mb-3">
-                        <label for="bankAccountNumber">Número de cuenta</label>
-                        <input type="text" class="form-control" id="bankAccountNumber" placeholder="" value="" >
-                        <div class="invalid-feedback">
-                          Ingresa un número de cuenta
-                        </div>
-                      </div>
-                </div>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
-              </div>
-            </div>
-          </div>
+            <p class="lead">Información del artículo</p>
         </div>
-        </div>
-    </main>
+        <h3>${articule.name}</h3>
+        <hr class="my-3">
+        <dl>
+            <dt>Cantidad</dt>
+            <dd><p>${articule.count}</p></dd>
+
+            <dt>Precio</dt>
+            <dd><p class="inline">${articule.currency} $ ${articule.cost}</p></dd>
+        </dl>
+    </div>
         ` ;
         document.getElementById("showCartArticules").innerHTML = htmlContentToAppend;
     
